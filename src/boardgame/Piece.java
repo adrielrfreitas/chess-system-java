9707aa5842bb3df8baa71b4d29a079f0a0fab5e1
Adriel Rober Freitas Fernandes <adriel.roberfreitas@outlook.com>
2024-06-20 10:06:42 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,23 @@
+package boardgame;
+
+public class Piece extends Board
+{
+
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board)
+    {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard()
+    {
+        return board;
+    }
+
+
+
+
+}
