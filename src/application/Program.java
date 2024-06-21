fb16d08f7ad274f82f7fc3fab41a1e977a0e3ecc
Adriel Rober Freitas Fernandes <adriel.roberfreitas@outlook.com>
2024-06-21 10:25:48 -0300

    Handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,37 +1,39 @@
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readyChessPosition(sc);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readyChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-
-            System.out.print("Do you want to continue? (yes/no): ");
-            String userResponse = sc.nextLine().trim().toLowerCase();
-            if (userResponse.equals("no")) {
-                break;
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch (ChessException | InputMismatchException e)
+            {
+               System.out.println(e.getMessage());
+               sc.nextLine();
             }
         }
-
-        sc.close();
     }
 }
