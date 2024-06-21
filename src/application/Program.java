cd66fd0a0d3e12eed267e4f1816038e73a09e5bc
Adriel Rober Freitas Fernandes <adriel.roberfreitas@outlook.com>
2024-06-21 10:10:09 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,37 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
-public class Program
-{
+import java.util.Scanner;
 
-    public static void main(String[] args)
-    {
+public class Program {
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
 
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readyChessPosition(sc);
 
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readyChessPosition(sc);
 
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 
+            System.out.print("Do you want to continue? (yes/no): ");
+            String userResponse = sc.nextLine().trim().toLowerCase();
+            if (userResponse.equals("no")) {
+                break;
+            }
+        }
 
-
+        sc.close();
     }
 }
