84dd4fa66446f183f1dc80ab9351766fe1a84a16
Adriel Rober Freitas Fernandes <adriel.roberfreitas@outlook.com>
2024-06-25 08:44:18 -0300

    Finish check logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -36,6 +36,11 @@ public class ChessMatch
         return currentPlayer;
     }
 
+    public boolean getCheck()
+    {
+        return check;
+    }
+
     public ChessPiece[][] getPieces()
     {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -63,6 +68,12 @@ public class ChessMatch
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
+        if (testCheck(currentPlayer))
+        {
+            undoMove(source, target, capturedPiece);
+            throw new ChessException("You can't put yourself in check");
+        }
+        check = (testCheck(opponent(currentPlayer))) ? true : false;
         nextTurn();
         return (ChessPiece) capturedPiece;
     }
@@ -142,6 +153,22 @@ public class ChessMatch
         }
         throw new IllegalStateException("There is no" + color + "king on the board");
     }
+
+    private boolean testCheck(Color color)
+    {
+        Position kingPosition = king(color).getChessPosition().toPosition();
+        List<Piece> opponentPieces = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == opponent (color)).collect(Collectors.toList());
+        for (Piece p : opponentPieces)
+        {
+            boolean[][] mat = p.possibleMoves();
+            if (mat[kingPosition.getRow()][kingPosition.getColumn()])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     private void placeNewPiece(char column, int row, ChessPiece piece)
     {
